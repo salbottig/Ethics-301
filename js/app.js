6b98746bed39059fe5b40224cfef54f89af2bb2e
Duke Ayers <Dfa24@nau.edu>
2014-10-05 13:04:36 -0700

    Spruced up the content
    Added AngularJS content and linked the CDN for Bootstrap so that we can
    delete it when it's ready. Also compacted the simple-sidebar CSS into
    the index file to get rid of the css file with it.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,110 @@
 var app = angular.module('ethicsApp', []);
 
 
+app.controller("dataCtrl",['$scope', '$http', function($scope, $http) {
+
+    $scope.newsFeed1 = 'False';
+    $scope.newsFeed2 = 'False';
+    $scope.beacon = 'False';
+    $scope.personalize = 'False';
+    $scope.button = 'False';
+    $scope.recog = 'False';
+    $scope.psychology = 'False';
+    $scope.conclusion = 'False';
+    $scope.statusArray = []
+
+    $scope.news1function = function(){
+        $scope.newsFeed1 = 'True';
+        $scope.newsFeed2 = 'False';
+        $scope.beacon = 'False';
+        $scope.personalize = 'False';
+        $scope.button = 'False';
+        $scope.recog = 'False';
+        $scope.psychology = 'False';
+        $scope.conclusion = 'False';
+    }
+
+    $scope.news2function = function(){
+        $scope.newsFeed1 = 'False';
+        $scope.newsFeed2 = 'True';
+        $scope.beacon = 'False';
+        $scope.personalize = 'False';
+        $scope.button = 'False';
+        $scope.recog = 'False';
+        $scope.psychology = 'False';
+        $scope.conclusion = 'False';
+    }
+
+    $scope.beaconFunc = function(){
+        $scope.newsFeed1 = 'False';
+        $scope.newsFeed2 = 'False';
+        $scope.beacon = 'True';
+        $scope.personalize = 'False';
+        $scope.button = 'False';
+        $scope.recog = 'False';
+        $scope.psychology = 'False';
+        $scope.conclusion = 'False';
+    }
+
+    $scope.personalizeFunc = function(){
+        $scope.newsFeed1 = 'False';
+        $scope.newsFeed2 = 'False';
+        $scope.beacon = 'False';
+        $scope.personalize = 'True';
+        $scope.button = 'False';
+        $scope.recog = 'False';
+        $scope.psychology = 'False';
+        $scope.conclusion = 'False';
+    }
+
+    $scope.buttonFunc = function(){
+        $scope.newsFeed1 = 'False';
+        $scope.newsFeed2 = 'False';
+        $scope.beacon = 'False';
+        $scope.personalize = 'False';
+        $scope.button = 'True';
+        $scope.recog = 'False';
+        $scope.psychology = 'False';
+        $scope.conclusion = 'False';
+    }
+
+    $scope.recogFunc = function(){
+        $scope.newsFeed1 = 'False';
+        $scope.newsFeed2 = 'False';
+        $scope.beacon = 'False';
+        $scope.personalize = 'False';
+        $scope.button = 'False';
+        $scope.recog = 'True';
+        $scope.psychology = 'False';
+        $scope.conclusion = 'False';
+    }
+    $scope.psychFunc = function(){
+        $scope.newsFeed1 = 'False';
+        $scope.newsFeed2 = 'False';
+        $scope.beacon = 'False';
+        $scope.personalize = 'False';
+        $scope.button = 'False';
+        $scope.recog = 'False';
+        $scope.psychology = 'True';
+        $scope.conclusion = 'False';
+    }
+    $scope.conclusionFunc = function(){
+        $scope.newsFeed1 = 'False';
+        $scope.newsFeed2 = 'False';
+        $scope.beacon = 'False';
+        $scope.personalize = 'False';
+        $scope.button = 'False';
+        $scope.recog = 'False';
+        $scope.psychology = 'False';
+        $scope.conclusion = 'True';
+    }
+
+    $scope.updateStatus = function(){
+       $scope.statusArray.push($scope.statusUpdate);
+       $scope.statusUpdate = '';
+    }
+
+
+
+}]);
+
